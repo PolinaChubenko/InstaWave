b7f7a83b696691f11deebd2ca598b7fd888aaee8
Polina Chubenko <chubenko.pn@phystech.edu>
2022-10-26 17:15:06 +0300

    states added

--- app/src/components/Auth/Forms/Forms.js
+++ app/src/components/Auth/Forms/Forms.js
@@ -2,38 +2,28 @@ import React from 'react';
 import SignInForm from "../SignInForm/SignInForm";
 import SignUpForm from "../SignUpForm/SignUpForm";
 
-class Forms extends React.Component {
-    handleClick = (e) => {
-        const switchers = [...document.querySelectorAll('.switcher')]
+const Forms = () => {
+    const [active_form, set_active_form] = React.useState("signin")
 
-        switchers.forEach(item => {
-            item.parentElement.classList.remove('is_active');
-        });
-
-        const id = e.target.id
-        document.getElementById(id).parentElement.classList.add('is_active')
-    }
-
-    render() {
-        return (
-            <div className="forms">
-                <div className="form_wrapper is_active">
-                    <button id="btn_sign_in" type="button" onClick={(e) => this.handleClick(e)}
-                            className="switcher switcher_login">
-                        Sign in
-                    </button>
-                    <SignInForm/>
-                </div>
-                <div className="form_wrapper">
-                    <button id="btn_sign_up" type="button" onClick={(e) => this.handleClick(e)}
-                            className="switcher switcher_signup">
-                        Sign Up
-                    </button>
-                    <SignUpForm/>
-                </div>
+    return (
+        <div className="forms">
+            <div className={"form_wrapper " + (active_form === "signin" ? "is_active" : " ")}>
+                <button id="btn_sign_in" type="button" onClick={() => set_active_form("signin")}
+                        className="switcher switcher_login">
+                    Sign in
+                </button>
+                <SignInForm/>
             </div>
-        );
-    }
+
+            <div className={"form_wrapper " + (active_form === "signup" ? "is_active" : " ")}>
+                <button id="btn_sign_up" type="button" onClick={() => set_active_form("signup")}
+                        className="switcher switcher_signup">
+                    Sign Up
+                </button>
+                <SignUpForm/>
+            </div>
+        </div>
+    );
 }
 
 export default Forms;
