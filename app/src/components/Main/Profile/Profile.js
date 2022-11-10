36af13f81bb9b79b479f626f1edf05f019c0ccee
Polina Chubenko <chubenko.pn@phystech.edu>
2022-11-11 02:10:04 +0300

    profile almost done

--- app/src/components/Main/Profile/Profile.js
+++ app/src/components/Main/Profile/Profile.js
@@ -1,10 +1,29 @@
 import Main from "../Main";
+import avatar from "./../../../images/main-background.jpg";
+import style from "./Profile.module.css";
 
 const Profile = () => {
     return (
         <Main>
-            <div>
-                Welcome home!
+            <div className={style.profile_wrapper}>
+                <div className={style.avatar_img}>
+                    <img alt='avatar' src={avatar}/>
+                </div>
+                <div className={style.info_wrapper}>
+                    <div className={style.username_block}>
+                        <p className={style.username}>lina._.po</p>
+                        <p className={style.subscribe}>Подписаться</p>
+                    </div >
+                    <div className={style.quote_block}>
+                        <p>какая-то очень умная цитата о том как крут vaprowave
+                            ну очень умная цитата о том как крут vaprowave</p>
+                    </div>
+                    <div className={style.publications_block}>
+                        <p>Публикаций: <span>10</span></p>
+                        <p>Подписчиков: <span>10</span></p>
+                        <p>Подписок: <span>10</span></p>
+                    </div>
+                </div>
             </div>
         </Main>
     )
