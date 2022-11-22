f79596ee3c402086a318a9fe55acc6e348a7febc
Polina Chubenko <chubenko.pn@phystech.edu>
2022-11-22 19:48:27 +0300

    likes and following added

--- back/back/settings.py
+++ back/back/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'post',
+    'following',
+    'like',
 ]
 
 MIDDLEWARE = [
--- back/post/models.py
+++ back/post/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
 from django.contrib.auth.models import User
 from blog.models import Blog
+from like.models import Like
 
 class Post(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="posts", null=True)
     date_creation = models.DateField(auto_now=True, editable=False)
+    likes = GenericRelation(Like)
+
+    @property
+    def total_likes(self):
+        return self.likes.count()
 
     def __str__(self) -> str:
-        return str(self.blog.user.username)
+        return f"created {str(self.date_creation)} by {self.blog.user.username} with {self.total_likes} likes"
